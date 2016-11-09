f4ec040013dd50176814b761e4fb1431af6f4d8b
Joon Park <parkjoon@umich.edu>
2016-11-09 12:55:54 -0500

    Minor style.

--- BetaKeyboard/KeyboardViewController.swift
+++ BetaKeyboard/KeyboardViewController.swift
@@ -226,9 +226,7 @@ class KeyboardViewController: UIInputViewController {
     
     func speakSelected() {
         let speechUtterance = AVSpeechUtterance(string: keyboardRows[selectedRowIndex][selectedCharIndex])
-        
         speechSynthesizer.stopSpeaking(at: AVSpeechBoundary.immediate)
-        
         speechSynthesizer.speak(speechUtterance)
     }
     
