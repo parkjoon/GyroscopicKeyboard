d9c534a09298f9c49c666bf97c08bf8f57e9c61e
user122224 <user122224@ny082.local>
2016-11-09 12:35:29 -0500

    Commented out speech

--- BetaKeyboard/KeyboardViewController.swift
+++ BetaKeyboard/KeyboardViewController.swift
@@ -62,7 +62,7 @@ class KeyboardViewController: UIInputViewController {
     }
     
     let manager = CMMotionManager()
-    let speechSynthesizer = AVSpeechSynthesizer()
+    //let speechSynthesizer = AVSpeechSynthesizer()
 
     func isGyroAvailable() {
         // Set the initially selected character.
@@ -205,7 +205,7 @@ class KeyboardViewController: UIInputViewController {
     func insertSelectedCharacter(_ sender: UITapGestureRecognizer){
         let selectedCharacter = keyboardRows[selectedRowIndex][selectedCharIndex]
         (textDocumentProxy as UIKeyInput).insertText(selectedCharacter)
-        speakSelected();
+        //speakSelected();
     }
     
     func enterDelete() {
@@ -216,13 +216,13 @@ class KeyboardViewController: UIInputViewController {
         (textDocumentProxy as UIKeyInput).insertText(" ")
     }
     
-    func speakSelected() {
-        let speechUtterance = AVSpeechUtterance(string: keyboardRows[selectedRowIndex][selectedCharIndex])
+    //func speakSelected() {
+    //    let speechUtterance = AVSpeechUtterance(string: keyboardRows[selectedRowIndex][selectedCharIndex])
         
-        speechSynthesizer.stopSpeaking(at: AVSpeechBoundary.immediate)
+    //    speechSynthesizer.stopSpeaking(at: AVSpeechBoundary.immediate)
         
-        speechSynthesizer.speak(speechUtterance)
-    }
+    //    speechSynthesizer.speak(speechUtterance)
+    //}
     
     /*
      * Helper for core functions.
