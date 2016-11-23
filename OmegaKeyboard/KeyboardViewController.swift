6c929f3ca9dde87b82f6a45849b73deffeeadce1
Mitchell Donahue <mrdonee@umich.edu>
2016-11-23 15:50:48 -0800

    improved shift left/right code

--- OmegaKeyboard/KeyboardViewController.swift
+++ OmegaKeyboard/KeyboardViewController.swift
@@ -171,19 +171,17 @@ class KeyboardViewController: UIInputViewController {
     }
     
     func shiftLeft() {
-        selectedCharIndex -= 1
-        if(selectedCharIndex < 0) {
-            selectedCharIndex = 0
+        if(selectedCharIndex > 0) {
+            selectedCharIndex -= 1
+            updateSelectionDisplay()
         }
-        updateSelectionDisplay()
     }
     
     func shiftRight() {
-        selectedCharIndex += 1
-        if(selectedCharIndex >= keyboardRows[selectedRowIndex].count) {
-            selectedCharIndex = keyboardRows[selectedRowIndex].count - 1
+        if(selectedCharIndex < keyboardRows[selectedRowIndex].count - 1) {
+            selectedCharIndex += 1
+            updateSelectionDisplay()
         }
-        updateSelectionDisplay()
     }
     
     func shiftUp() {
